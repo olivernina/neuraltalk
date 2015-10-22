507005269c91edd2d66e7a757c16461dbfdf8fdf
oliver <olivernina@gmail.com>
2015-10-22 17:06:59 -0400

    add input coupled

--- imagernn/generic_batch_generator.py
+++ imagernn/generic_batch_generator.py
@@ -8,7 +8,8 @@ from imagernn.noinput_generator import NOINPUTGenerator
 from imagernn.noforget_generator import NOFORGETGenerator
 from imagernn.icoupled_generator import ICOUPLEDGenerator
 from imagernn.fcoupled_generator import FCOUPLEDGenerator
-
+from imagernn.iorem_generator import IOREMGenerator
+from imagernn.icorem_generator import ICOREMGenerator
 
 def decodeGenerator(generator):
   if generator == 'lstm':
@@ -25,6 +26,10 @@ def decodeGenerator(generator):
     return ICOUPLEDGenerator
   if generator == 'fcoupled':
     return FCOUPLEDGenerator
+  if generator == 'iorem':
+    return IOREMGenerator
+  if generator == 'icorem':
+    return ICOREMGenerator
   else:
     raise Exception('generator %s is not yet supported' % (base_generator_str,))
 
